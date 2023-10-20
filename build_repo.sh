5143577ae947cb4bb4538fe2b2bb6793afb4e652
ading2210 <ading2019@gmail.com>
2023-10-19 22:57:05 -0400

    make dpkg-scanpackages use relative paths

--- build_repo.sh
+++ build_repo.sh
@@ -10,22 +10,24 @@ fi
 
 supported_releases="bookworm unstable"
 base_path="$(realpath $(pwd))"
+repo_dir="$base_path/repo/"
 
-rm -rf repo || true
-mkdir -p repo
+rm -rf $repo_dir || true
+mkdir -p $repo_dir
 
 for release_name in $supported_releases; do
   cd $base_path
 
   . ./build.sh $release_name
-  pool_dir="$base_path/repo/pool/main/$release_name"
-  dists_dir="$base_path/repo/dists/$release_name/main/binary-amd64"
+  pool_dir="$repo_dir/pool/main/$release_name"
+  dists_dir="$repo_dir/dists/$release_name/main/binary-amd64"
   
   mkdir -p $dists_dir
   mkdir -p $pool_dir
 
   cp $base_path/build/*.deb $pool_dir
-  dpkg-scanpackages --arch amd64 $pool_dir > $dists_dir/Packages
+  cd $repo_dir
+  dpkg-scanpackages --arch amd64 pool/main/$release_name > $dists_dir/Packages
   cat $dists_dir/Packages | gzip -9 > $dists_dir/Packages.gz
 done
 
